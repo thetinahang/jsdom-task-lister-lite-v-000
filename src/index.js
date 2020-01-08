a3c0a2290c4dec8cc92a3f12bcc81280d96f5f76
Tina Hang <mstinahang@gmail.com>
2020-01-08 03:58:10 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("submit-form-btn").addEventListener("click", e => {
+    e.preventDefault();
+    createTodo();
+  });
 });
+
+function createTodo() {
+  let taskText = document.getElementById("new-task-description").value;
+  let newTask = document.createElement("li");
+  newTask.innerHTML =
+    taskText +
+    "&nbsp;" +
+    "<button class='delete' onclick='deleteTask(this)'>x</button>";
+  document.getElementById("tasks").appendChild(newTask);
+}
+
+function deleteTask(task) {
+  task.parentNode.remove();
+}
