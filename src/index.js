e367792b602adcef13f131f0754cc1f1dfea53f6
Tina Hang <mstinahang@gmail.com>
2020-01-08 04:14:06 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  initialize();
 });
+
+function initialize() {
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', addToDo);
+}
+
+function addToDo(e) {
+  e.preventDefault();
+  const task = document.getElementById('new-task-description');
+  addToList(task.value);
+  text.value = "";
+}
+
+function addToList(task){
+  const list = document.getElementById('tasks');
+  const item = document.createElement('li');
+  item.innerHTML = task;
+  list.appendChild(item);
+}
