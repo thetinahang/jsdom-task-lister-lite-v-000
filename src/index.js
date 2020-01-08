936928105df413658df15086cbe75a1e9f3ab3bc
Tina Hang <mstinahang@gmail.com>
2020-01-08 04:10:51 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  document.getElementById("submit-form-btn").addEventListener("click", e => {
-    e.preventDefault();
-    createTodo();
-  });
+  document.querySelector("form").addEventListener("submit", function (event) {
+    value = document.getElementById('new-task-description').value
+    newItem = document.createElement('li');
+    newItem.innerText = `${value}`;
+    document.getElementById("tasks").appendChild(newItem);
+    event.preventDefault();
+    document.getElementById('new-task-description').value = ``;
+  })
 });
-
-function createTodo() {
-  let taskText = document.getElementById("new-task-description").value;
-  let newTask = document.createElement("li");
-  newTask.innerHTML =
-    taskText +
-    "&nbsp;" +
-    "<button class='delete' onclick='deleteTask(this)'>x</button>";
-  document.getElementById("tasks").appendChild(newTask);
-}
-
-function deleteTask(task) {
-  task.parentNode.remove();
-}
